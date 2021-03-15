07caf45ec7f6061368701d1f215feaa0baf962fc
DESKTOP-NUOPUIF\ITPS <jyongju928@gmail.com>
2021-03-15 17:19:05 +0900

    최초 프로젝트 업로드

---
+++ src/chapter01/Ex01.java
@@ -0,0 +1,18 @@
+package chapter01;
+
+// 클래스 : 소스 코드의 최소 단위
+public class Ex01 {
+	// 메인 함수 : 시작점, 내가 작성한 소스 코드의 시작이 어딘지 알려주는 기능  
+	public static void main(String[] args) {
+		// 주석 : 메모, 개발자가 참고해야 할  사항
+		
+		// 무언가를 출력하기 위한 기능
+		// ( ) 안에다가 출력 대상을 지정
+		System.out.println("Hello World!");		// Hello World! 문자열 출력
+		System.out.println("주석 연습 중...");		// 주석 연습 중... 출력
+		
+		
+		
+	} // end main
+
+}
---
+++ src/chapter01/Ex02.java
@@ -0,0 +1,44 @@
+package chapter01;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// 문자열 - 문자의 나열
+		// 문자열은 꼭 쌍따옴표를사용해야 표현해야 한다.
+		
+//		System.out.println("Hello World~!");
+		
+//		System.out.println("Hello ");
+//		System.out.println("World~!");
+		
+//		System.out.println("I am");
+//		System.out.println("a");
+//		System.out.println("Java Programmer");
+
+//		System.out.print("오늘은 ");
+//		System.out.print("자바 수업 ");
+//		System.out.print("첫 번째 날입니다.");
+		
+//		System.out.print("I am ");
+//		System.out.print("a ");
+//		System.out.print("Java Programmer");
+		
+//		// 정수
+//		System.out.println(1);
+//		// 실수
+//		System.out.println(3.14);
+//		// 문자
+//		System.out.println('a');
+//		// 문자열
+//		System.out.println("Hello");
+		
+		System.out.println("오늘의 기온 : " + (-5.3) + "도");
+		System.out.println("커피 가격 : " + 1500 );
+		System.out.println("철수의 Java 과목 학점 : " + 'A');
+		
+	
+		
+		
+	} // end main
+
+}
